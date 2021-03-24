c772ba9b06ef464411a4d716cda402f5a693f2e0
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-03-24 15:13:16 +0530

    Update play.ts

--- src/handlers/play.ts
+++ src/handlers/play.ts
@@ -1,5 +1,7 @@
-import { Composer } from 'telegraf';
+import { Composer, deunionize } from 'telegraf';
 import { addToQueue } from '../tgcalls';
+import { getCurrentSong } from '../tgcalls';
+import escapeHtml from '@youtwitface/escape-html';
 
 export const playHandler = Composer.command('play', async ctx => {
     const { chat } = ctx.message;
@@ -10,7 +12,7 @@ export const playHandler = Composer.command('play', async ctx => {
     }
 
     const [commandEntity] = ctx.message.entities!;
-    const text = ctx.message.text.slice(commandEntity.length + 1);
+    const text = ctx.message.text.slice(commandEntity.length + 1) || deunionize(ctx.message.reply_to_message)?.text;
 
     if (!text) {
         await ctx.reply('You need to specify a YouTube URL.');
@@ -18,6 +20,42 @@ export const playHandler = Composer.command('play', async ctx => {
     }
 
     const index = await addToQueue(chat, text);
+    const song = getCurrentSong(chat.id);
+    
+    let time = '';
 
-    await ctx.reply(index === 0 ? `Playing\n${text}` : `Queued ${text} at position ${index}.`);
+    if (song.duration > 0) {
+        time += ' - ';
+
+        const hours = Math.floor(song.duration / 3600);
+        const minutes = Math.floor(song.duration / 60) % 60;
+        const seconds = (song.duration % 60).toString().padStart(2, '0');
+
+        if (hours > 0) {
+            time += `${hours}:${minutes.toString().padStart(2, '0')}:${seconds}`;
+        } else {
+            time += `${minutes}:${seconds}`;
+        }
+    }
+
+    soong = `<b>Title:<b> <a href="https://youtu.be/${song.id}">${escapeHtml(song.title)}</a><break><b>Duration:</b> ${time}`;
+
+    let message;
+
+    switch (index) {
+        case -1:
+            message = 'Failed to download song.';
+            break;
+
+        case 0:
+            message = `<b>Playing </b><break>${soong}`;
+            break;
+
+        default:
+            message = `<b>Queued</b><break>${soong}<break><b>at position ${index}.</b>`;
+    }
+
+    await ctx.reply(message, {
+        parse_mode: 'HTML',
+    });
 });
