e47da54de286f784e57f4f48feac8887116e266e
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-03-24 13:52:47 +0530

    Update index.ts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,10 +4,12 @@ import { playHandler } from './play';
 import { queueHandler } from './queue';
 import { pauseHandler } from './pause';
 import { skipHandler } from './skip';
+import { somgHandler } from './song';
 
 export const initHandlers = (): void => {
     bot.use(playHandler);
     bot.use(queueHandler);
     bot.use(pauseHandler);
+    bot.use(skipHandler);
     bot.use(skipHandler);
 };
