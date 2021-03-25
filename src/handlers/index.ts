5c9b130ef20567c13086104e83022bad01db1ea3
ArnabXD <arnabxd@pm.me>
2021-03-25 11:09:19 +0530

    use PauseCallback

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,14 +2,14 @@ import { bot } from '../bot';
 
 import { playHandler } from './play';
 import { queueHandler } from './queue';
-import { pauseHandler } from './pause';
+import { pauseHandler, pauseCBHandler } from './pause';
 import { skipHandler, skipCallBack } from './skip';
 import { songHandler } from './current';
 
 export const initHandlers = (): void => {
     bot.use(playHandler);
     bot.use(queueHandler);
-    bot.use(pauseHandler);
+    bot.use(pauseHandler, pauseCBHandler);
     bot.use(skipHandler, skipCallBack);
     bot.use(songHandler);
 };
