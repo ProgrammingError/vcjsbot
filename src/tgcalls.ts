5f7e155518f7aed942c0d2d81a6f01f5a160be36
ArnabXD <arnabxd@pm.me>
2021-03-25 16:08:13 +0530

    sec -> Xm Xs

--- src/tgcalls.ts
+++ src/tgcalls.ts
@@ -7,6 +7,7 @@ import WebSocket from 'ws';
 import { Readable } from 'stream';
 import { bot } from './bot';
 import { Markup } from 'telegraf';
+import { getDuration } from './utils';
 
 interface DownloadedSong {
     stream: Readable;
@@ -126,15 +127,16 @@ const createConnection = async (chat: Chat.SupergroupChat): Promise<void> => {
             const url = queue.shift()!;
             try {
                 const song = await downloadSong(url);
-                const { title, id } = song.info
+                const { title, id, duration } = song.info
                 stream.setReadable(song.stream);
                 cachedConnection.currentSong = song.info;
                 bot.telegram.sendPhoto(chat.id, `https://img.youtube.com/vi/${id}/mqdefault.jpg`, {
-                    caption: `<b>Playing : </b> <a href="https://www.youtube.com/watch?v=${id}">${title}</a>\n<b>Duration: </b>${song.info.duration}`,
+                    caption: `<b>Playing : </b> <a href="https://www.youtube.com/watch?v=${id}">${title}</a>\n` +
+                        `<b>Duration: </b>${getDuration(duration)}`,
                     parse_mode: 'HTML',
                     ...Markup.inlineKeyboard([
                         [
-                            Markup.button.callback('Pause',`pause:${id}`),
+                            Markup.button.callback('Pause', `pause:${id}`),
                             Markup.button.callback('Skip', `skip:${id}`)
                         ]
                     ])
