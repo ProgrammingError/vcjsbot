2f5f449c7d12c5a3211313a59803800295c98ed0
ArnabXD <arnabxd@pm.me>
2021-03-26 09:36:30 +0530

    DEBUG MODE 6969

--- src/handlers/pause-resume.ts
+++ src/handlers/pause-resume.ts
@@ -3,28 +3,33 @@ import { pause, getCurrentSong } from '../tgcalls';
 import { getDuration } from '../utils';
 import escapeHtml from '@youtwitface/escape-html';
 import checkExpired from '../middlewares/checkExpired';
+import { logger as log } from '../bot';
 
 export const pauseCBHandler = Composer.action(/^pause:[a-zA-Z0-9]+$/, checkExpired, async ctx => {
+    await log("Received Callback");
     const chat = ctx.callbackQuery.message?.chat;
 
     let data: string = '';
     if ('data' in ctx.callbackQuery) data = ctx.callbackQuery.data;
-    data = data.split(":")[1];
+    await log(`CB data : ${data}`)
 
     if (!chat) {
         await ctx.answerCbQuery("Invalid Request");
         return false;
     }
 
-    const paused = pause(chat.id);
     const current = getCurrentSong(chat.id);
+    const paused = pause(chat.id);
+    await log(`Paused`);
     if (!current) {
+        await log("NOTHING IS PLAYING");
         await ctx.answerCbQuery("There's nothing playing here.");
         return setTimeout(async () => await ctx.deleteMessage(), 1000);
     }
 
     const { id, title, duration } = current;
     if (paused) {
+        await log("EDITING MESSAGE ...");
         await ctx.editMessageCaption(`<b>Paused :</b> <a href="https://www.youtube.com/watch?v=${id}">${escapeHtml(title)}</a>\n` +
             `<b>Duration :</b> ${getDuration(duration)}\n` +
             `<b>Paused by :</b> <a href="tg://user?id=${ctx.from?.id}">${ctx.from?.first_name}</a>`, {
@@ -38,6 +43,7 @@ export const pauseCBHandler = Composer.action(/^pause:[a-zA-Z0-9]+$/, checkExpir
         });
         return await ctx.answerCbQuery("Paused ...");
     } else {
+        await log("EDITING MESSAGE ...");
         await ctx.editMessageCaption(`<b>Playing :</b> <a href="https://www.youtube.com/watch?v=${id}">${escapeHtml(title)}</a>\n` +
             `<b>Duration :</b> ${getDuration(duration)}\n` +
             `<b>Resumed by :</b> <a href="tg://user?id=${ctx.from?.id}">${ctx.from?.first_name}</a>`, {
