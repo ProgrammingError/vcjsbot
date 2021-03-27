b9d0fa57d042dd9eb275bfa2e31e4108743bfbeb
ArnabXD <arnabxd@pm.me>
2021-03-27 11:49:32 +0530

    use logger

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { bot } from './bot';
+import { bot, logger as log } from './bot';
 import { initHandlers } from './handlers';
 import { initMiddleWares } from './middlewares';
 import env from './env';
@@ -8,5 +8,5 @@ import env from './env';
     initHandlers();
     await bot.telegram.deleteWebhook({ drop_pending_updates: true });
     await bot.launch();
-    await bot.telegram.sendMessage(env.LOG_CHANNEL, `@${bot.botInfo?.username} is running...`);
+    await log(`@${bot.botInfo?.username} is running...`);
 })();
--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -2,11 +2,11 @@ import { Context, MiddlewareFn } from 'telegraf';
 import { logger as log } from '../bot';
 import escapeHtml from '@youtwitface/escape-html';
 
-const Logger: MiddlewareFn<Context> = async (ctx, next) => {
+const Logger: MiddlewareFn<Context> = async (_, next) => {
     try {
         await next();
     } catch (err) {
-        await log(`<b>Error :</b> ${escapeHtml(err.toString())}`, "HTML");
+        await log(`<b>Error :</b>\n <code>${escapeHtml(err.toString())}</code>`, "HTML");
     }
 }
 
