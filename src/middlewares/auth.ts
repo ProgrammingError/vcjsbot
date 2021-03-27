eb781f0fdcdf732c67be6fac69fda0f4c01e0493
ArnabXD <arnabxd@pm.me>
2021-03-27 11:11:11 +0530

    auth simplified

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,7 +2,6 @@ import { Context, MiddlewareFn } from 'telegraf';
 import client from '../redis';
 import { promisify } from 'util';
 
-const regex = /^\/([^@\s]+)@?(?:(\S+)|)\s?([\s\S]+)?$/i; // Credit -> https://github.com/telegraf/telegraf-command-parts/blob/da799b344b723e09c0c936bd5fbdd344bda4033e/index.js#L3
 const getAsync = promisify(client.get).bind(client);
 
 const Auth: MiddlewareFn<Context> = async (ctx, next) => {
@@ -25,14 +24,11 @@ const Auth: MiddlewareFn<Context> = async (ctx, next) => {
     if ((id && sudos && (typeof (sudos) === 'object') && sudos.includes(id)) || (id === owner)) {
         return next();
     } else {
-        if (ctx.message && 'text' in ctx.message && regex.exec(ctx.message.text)) {
-            return;
-        } else if (ctx.callbackQuery) {
+        if (ctx.callbackQuery) {
             return ctx.answerCbQuery("You aren't authorized ...", {
                 show_alert: true
             })
-        }
-        else {
+        } else {
             return;
         }
     }
